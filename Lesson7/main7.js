10f96e93481a5ad0c85caf86f98ca5918b7bf54b
VitaliyKrymov <krymvitaliy@gmail.com>
2021-11-30 23:31:24 +0200

    Homework to Lesson7 90%

--- Lesson7/main7.js
+++ Lesson7/main7.js
@@ -47,31 +47,29 @@
 
 // - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
 
-// class Client {
-//     constructor(id, name, surname, email, phone, order) {
-//         this.id = id || (Math.floor(Math.random() * 100));
-//         this.name = name || 'noName';
-//         this.surname = surname || 'noSurname';
-//         this.email = email || 'noEmail@ukr.net';
-//         this.phone = phone || '+380950000000';
-//         this.order = order || ['water', 'bred', 'C2H5OH'];
+// class Client{
+//     constructor(id, name, surname , email, phone, order) {
+//         this.id=id||(Math.floor(Math.random()*100));
+//         this.name=name||'noName';
+//         this.surname=surname||'noSurname';
+//         this.email=email||'noEmail@ukr.net';
+//         this.phone=phone||'+380950000000';
+//         this.order=order|| ['water','bred','C2H5OH'];
 //     }
 // }
-
-// створити пустий масив, наповнити його 10 об'єктами Client
-// let clients = [];
-// clients.push(new Client(1, 'Viktor', 'Pupkin', 'pupV@gmail.com', '+380952356731', ['bread', 'milk', 'meat', 'juse', 'apples', 'cake', 'shuger', 'vine', 'rom', 'bear']));
-// clients.push(new Client(8, 'Vasiliy', 'Petpov', 'petrov@gmail.com', '+380952356732', [, 'milk', 'meat', 'juse', 'apples', 'cake', 'shuger', 'vine', 'rom', 'bear']));
-// clients.push(new Client(3, 'Viacheslav', 'Smith', 'smithV@gmail.com', '+380632356730', ['bread', 'milk', 'chicken', 'juse', 'apples', 'cake', 'shuger', 'vine', 'rom', 'bear']));
-// clients.push(new Client(6, 'Vladimir', 'Ivanov', 'Ivanov@gmail.com', '+380632356535', ['water', 'meat', 'juse', 'apples', 'bear']));
-// clients.push(new Client(5, 'Valentin', 'Ermolin', 'Valentin@gmail.com', '+380632356730', ['bread', 'milk', 'meat', 'chicken', 'juse', 'apples', 'cake', 'shuger', 'vine', 'rom', 'bear']));
-// clients.push(new Client(2, 'Oleg', 'Petrov', 'Oleg@gmail.com', '+380632356730', ['bread', 'milk', 'meat', 'juse', 'apples', 'cake', 'vine', 'bear']));
-// clients.push(new Client(7, 'Taras', 'Sergeev', 'Taras456@gmail.com', '+380632356730', ['vine', 'rom', 'bear']));
-// clients.push(new Client(4, 'Georgiy', 'Panov', 'Georgiy34@gmail.com', '+380634356730', ['bread', 'milk']));
-// clients.push(new Client(9, 'Ivan', 'Nazarenko', 'Ivan1987@gmail.com', '+380632456930', ['bread', 'water', 'chicken', 'cake', 'rom',]));
-// clients.push(new Client(10, 'Nikolay', 'Shestak', 'Nikolay23@gmail.com', '+380632396739', ['bread', 'milk', 'meat', 'apples', 'cake', 'vine', 'rom', 'bear']));
+// // створити пустий масив, наповнити його 10 об'єктами Client
+// let clients=[];
+// clients.push(new Client(1,'Viktor','Pupkin','pupV@gmail.com','+380952356731',['bread','milk','meat','juse','apples','cake','shuger','vine','rom','bear']));
+// clients.push(new Client(8,'Vasiliy','Petpov','petrov@gmail.com','+380952356732',[,'milk','meat','juse','apples','cake','shuger','vine','rom','bear']));
+// clients.push(new Client(3,'Viacheslav','Smith','smithV@gmail.com','+380632356730',['bread','milk','chicken','juse','apples','cake','shuger','vine','rom','bear']));
+// clients.push(new Client(6,'Vladimir','Ivanov','Ivanov@gmail.com','+380632356535',['water','meat','juse','apples','bear']));
+// clients.push(new Client(5,'Valentin','Ermolin','Valentin@gmail.com','+380632356730',['bread','milk','meat','chicken','juse','apples','cake','shuger','vine','rom','bear']));
+// clients.push(new Client(2,'Oleg','Petrov','Oleg@gmail.com','+380632356730',['bread','milk','meat','juse','apples','cake','vine','bear']));
+// clients.push(new Client(7,'Taras','Sergeev','Taras456@gmail.com','+380632356730',['vine','rom','bear']));
+// clients.push(new Client(4,'Georgiy','Panov','Georgiy34@gmail.com','+380634356730',['bread','milk']));
+// clients.push(new Client(9,'Ivan','Nazarenko','Ivan1987@gmail.com','+380632456930',['bread','water','chicken','cake','rom',]));
+// clients.push(new Client(10,'Nikolay','Shestak','Nikolay23@gmail.com','+380632396739',['bread','milk','meat','apples','cake','vine','rom','bear']));
 // console.log(clients);
-
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 // let sortedClientsByOrder = clients.sort((client1, client2) => client1.order.length - client2.order.length);
 // console.log(sortedClientsByOrder);
@@ -189,37 +187,39 @@
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
 
 
-class Popelushka{
-    constructor(name,age,sizeFoot) {
-        this.name=name||'No name';
-        this.age=age||99;
-        this.sizeFoot=sizeFoot||42;
-    }
-}
+// class Popelushka {
+//     constructor(name, age, sizeFoot) {
+//         this.name = name || 'No name';
+//         this.age = age || 99;
+//         this.sizeFoot = sizeFoot || 42;
+//     }
+// }
+
 // --Створити 10 попелюшок , покласти їх в масив
-const Kristi=new Popelushka('Kristi',19,37);
-const Katia=new Popelushka('Katia',20,38);
-const Karina=new Popelushka('Karina',21,39);
-const Klara=new Popelushka('Klara',22,37);
-const Klava=new Popelushka('Klava',21,36);
-const Kira=new Popelushka('Kira',23,39);
-const Olia=new Popelushka('Olia',24,38);
-const Tania=new Popelushka('Tania',26,39);
-const Sveta=new Popelushka('Sveta',15,38);
-const Sasha=new Popelushka('Sasha',18,36);
-
-let PopelushkaArr=[Kristi,Katia,Karina,Klara,Klava,Kira,Olia,Tania,Sveta,Sasha];
-console.log(PopelushkaArr);
-
-// --Створити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
-class Prins {
-    constructor(name,age,sizeSlipper) {
-        this.name=name||'No name';
-        this.age=age||99;
-        this.sizeSlipper=sizeSlipper||42;
-    }
-}
-const PrinsVasia = new Prins('Vasia',21,39);
+// const Kristi = new Popelushka('Kristi', 19, 37);
+// const Katia = new Popelushka('Katia', 20, 38);
+// const Karina = new Popelushka('Karina', 21, 39);
+// const Klara = new Popelushka('Klara', 22, 37);
+// const Klava = new Popelushka('Klava', 21, 36);
+// const Kira = new Popelushka('Kira', 23, 39);
+// const Olia = new Popelushka('Olia', 24, 38);
+// const Tania = new Popelushka('Tania', 26, 39);
+// const Sveta = new Popelushka('Sveta', 15, 38);
+// const Sasha = new Popelushka('Sasha', 18, 36);
+//
+// let PopelushkaArr = [Kristi, Katia, Karina, Klara, Klava, Kira, Olia, Tania, Sveta, Sasha];
+// console.log(PopelushkaArr);
+//
+// // --Створити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+// class Prins {
+//     constructor(name, age, sizeSlipper) {
+//         this.name = name || 'No name';
+//         this.age = age || 99;
+//         this.sizeSlipper = sizeSlipper || 42;
+//     }
+// }
+//
+// const PrinsVasia = new Prins('Vasia', 21, 39);
 
 // -- за допоиоги циклу знайти яка попелюшка повинна бути з принцом
 // let i=0;
@@ -231,12 +231,12 @@ const PrinsVasia = new Prins('Vasia',21,39);
 //              }
 // if (i===0){ console.log(`Prins ${PrinsVasia.name} dont find Popelushka`)};
 // if (i>1){ console.log(`Prins ${PrinsVasia.name} don't know what to do???`)};
-//
+
 
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
 
-
-
+// const emptyPrincess = PopelushkaArr.find((item) => item.sizeFoot === PrinsVasia.sizeSlipper);
+// console.log(emptyPrincess);
 
 
 //ADDITIONAL
@@ -245,9 +245,7 @@ const PrinsVasia = new Prins('Vasia',21,39);
 // - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
 // Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
 
-// {
-//     id: 1,
-//         name: 'Leanne Graham',
+// let arr={   id: 1,  name: 'Leanne Graham',
 //     username: 'Bret',
 //     email: 'Sincere@april.biz',
 //     address: {
@@ -268,6 +266,34 @@ const PrinsVasia = new Prins('Vasia',21,39);
 //         bs: 'harness real-time e-markets'
 // }
 // }
+
+class Users {
+    street;
+    lat;
+    lng;
+
+    constructor(
+        {
+            id,
+            name = '',
+            username = '',
+            email = '',
+    address ={
+                this.street,
+        this.suite,
+        this.city,
+        this.zipcode,
+        this.geo = {this.lat,this.lng}
+            },
+            phone,
+            website,
+            company = {name, catchPhrase, bs}
+        })
+}
+
+let user1 = new Users(2,'vasia pupkin','VasKin','pup@gmail.com',addres:{'kharkivsra street', 'Apt. 67', 'Greenborn', ''});
+
+
 //
 //
 // -  Створити функцію конструктор / клас  який описує об'єкт тегу
@@ -301,3 +327,76 @@ const PrinsVasia = new Prins('Vasia',21,39);
 // ]
 //
 // }
+// function Tegs(titleOfTag, action, attrs) {
+//     this.titleOfTag = titleOfTag || 'no name';
+//     this.action = action || 'no descriptionTag';
+//     this.attrs = attrs || 'no attributs';
+// }
+//
+// let title = '<a>'
+// let action = 'Tег <a>  предназначен для создания ссылок,устанавливает ссылку или якорь.';
+// let atributes = [
+//     {titleOfAttr: 'accesskey', actionOfAttr: 'Активация ссылки с помощью комбинации клавиш.'},
+//     {titleOfAttr: 'coords', actionOfAttr: 'Устанавливает координаты активной области.'},
+//     {titleOfAttr: 'download', actionOfAttr: 'Предлагает скачать указанный по ссылке файл.'},
+// ];
+// let a = new Tegs(title, action, atributes);
+// console.log(a);
+// let div = new Tegs('<div>', 'Элемент <div> является блочным элементом и предназначен для выделения фрагмента' +
+//     ' документа с целью изменения вида содержимого.', [{
+//     titleOfAttr: 'align',
+//     actionOfAttr: 'Задает выравнивание содержимого тега <div>.'
+// }, {titleOfAttr: 'title', actionOfAttr: 'Добавляет всплывающую подсказку к содержимому.'}]);
+// console.log(div);
+// let h1 = new Tegs('<h1>', 'Тег <h1> представляет собой наиболее важный заголовок первого уровня. ' +
+//     "По умолчанию, заголовок первого уровня отображается самым крупным шрифтом жирного начертания.", [{
+//     titleOfAttr: 'align',
+//     actionOfAttr: 'Определяет выравнивание заголовка.'
+// },]);
+// console.log(h1);
+// let span = new Tegs('<span>', 'Тег <span> предназначен для определения строчных элементов документа. В отличие от' +
+//     ' блочных элементов, таких как <table>, <p> или <div>, с помощью тега <span> можно выделить часть информации внутри других тегов и установить для нее свой стиль.', [{
+//     titleOfAttr: 'accesskey',
+//     actionOfAttr: 'Позволяет получить доступ к элементу с помощью заданного сочетания клавиш.'
+// }, {
+//     titleOfAttr: 'class',
+//     actionOfAttr: 'Определяет имя класса, которое позволяет связать тег со стилевым оформлением.'
+// }]);
+// console.log(span);
+// let input = new Tegs('<input>', 'Тег <input> является одним из разносторонних элементов формы и ' +
+//     'позволяет создавать разные элементы интерфейса и обеспечить взаимодействие с пользователем. Главным образом <input> ' +
+//     'предназначен для создания текстовых полей, различных кнопок, переключателей и флажков.', [{
+//     titleOfAttr: 'accesskey',
+//     actionOfAttr: 'Позволяет получить доступ к элементу с помощью заданного сочетания клавиш.'
+// }, {
+//     titleOfAttr: 'class',
+//     actionOfAttr: 'Определяет имя класса, которое позволяет связать тег со стилевым оформлением.'
+// }]);
+// console.log(input);
+// let form = new Tegs('<form>', 'Тег <form> Связывает поле с формой по её идентификатору.', [{
+//     titleOfAttr: '',
+//     actionOfAttr: ''
+// },]);
+// console.log(form);
+// let option = new Tegs('<option>', 'Тег <option> определяет отдельные пункты списка, создаваемого с помощью контейнера <select>.', [{
+//     titleOfAttr: 'disabled',
+//     actionOfAttr: 'Заблокировать для доступа элемент списка.'
+// }, {
+//     titleOfAttr: 'label',
+//     actionOfAttr: 'Указание метки пункта списка.'
+// }, {
+//     titleOfAttr: 'selected',
+//     actionOfAttr: 'Заранее устанавливает определенный пункт списка выделенным.'
+// }]);
+// console.log(option);
+// let select = new Tegs('<select>', 'Тег <select> позволяет создать элемент интерфейса в виде раскрывающегося списка, а также список с одним или множественным выбором, как показано далее. Конечный вид зависит от использования атрибута size тега <select>, который устанавливает высоту списка.', [{
+//     titleOfAttr: 'accesskey',
+//     actionOfAttr: 'Позволяет перейти к списку с помощью некоторого сочетания клавиш.'
+// }, {
+//     titleOfAttr: 'autofocus',
+//     actionOfAttr: 'Устанавливает, что список получает фокус после загрузки страницы.'
+// }, {
+//     titleOfAttr: 'disabled',
+//     actionOfAttr: 'Блокирует доступ и изменение элемента.'
+// }]);
+// console.log(select);
